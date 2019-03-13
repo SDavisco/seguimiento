d162dcc90383e41a5cca310fed2a7dc3d4e7454d
SDavisco <sistemasweb@davisco.com.pe>
2019-03-13 17:07:23 -0500

    prueba de validar.php

--- validar.php
+++ validar.php
@@ -1,25 +1,26 @@
 <?php
 	require_once('conexion.php');
     date_default_timezone_set('America/Lima');
+    $user_name = $_POST['user'];
     $cli_nomb=$_POST['cliente'];
     $cli_cont = $_POST['cli_cont']; 
     $cli_mail=$_POST['cliente_mail'];
     $cli_type=$_POST['cli_type'];
+    $emp_nomb= "DAVISCO S.A";
     $fecha = $_POST['inc_fecha'];
+    $type_sg = $_POST['type_sg'];
     $fechaBD = date("d-m-Y", strtotime($fecha));
     $inc_fecha= date('d/m/y g:ia');
     $inc_proc=$_POST['proceso'];
-    $inc_obs_c=$_POST['cliente_obs'];
-    $inc_obs_v=$_POST['vendedor_obs'];
-    $emp_nomb= "DAVISCO S.A";
+    $inc_obs=$_POST['obs'];
     var_dump($_POST);
     echo $inc_fecha ;
-/*	$sql="INSERT INTO INCIDENCIAS (user_name, cli_nomb, cli_cont, cli_mail, cli_type, emp_nomb, correo_fecha  inc_fecha, inc_proc, inc_obs_c, inc_obs_v) VALUES ('$user_name', '$cli_nomb', '$cli_cont', '$cli_mail','$cli_type', '$emp_nomb', '$fechaBD, '$inc_fecha','$inc_proc', '$inc_obs_c', '$inc_obs_v')"; 
+	$sql="INSERT INTO INCIDENCIAS (user_name, cli_nomb, cli_cont, cli_mail, cli_type, emp_nomb, correo_fecha, incidencia_fecha, canal_seguimiento, inc_proc, obs_general ) VALUES ('$user_name', '$cli_nomb', '$cli_cont', '$cli_mail','$cli_type', '$emp_nomb', '$inc_fecha', '$fechaBD', '$inc_proc', '$inc_obs')"; 
 if (mysqli_query($conn, $sql))
 {
     echo "Exito";
 }
 else{
     echo "error". $sql."<br>".mysqli_error($conn);
-}*/
-?>
+}
+?> 
